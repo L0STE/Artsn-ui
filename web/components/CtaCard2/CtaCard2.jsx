6d8fdedfb17467434c2357e3419db7d9b946ee0e
Moazzam Ali <moazzamali@Moazzams-MacBook-Pro.local>
2024-04-26 01:32:36 +0500

    desktop landing

--- web/components/CtaCard2/CtaCard2.jsx
+++ web/components/CtaCard2/CtaCard2.jsx
@@ -1,25 +1,20 @@
-import "@/styles/CtaCard2.scss";
+import '@/styles/CtaCard2.scss';
 
 const CTA2Card = () => {
-    return (
-        <div className="cta2-card ">
-            <div className="cta2-card__text">
-                <h2 className="top-text">Diversify your Portfolio</h2>
-                <h3 className="bottom-text">Collect Real World Assets</h3>
-                <p className="sub-text">
-                    Welcome in our high-end ownership and trade platform.
-                </p>
-            </div>
-            <button className="btn-cta-2 uppercase">
-                Start Now
-            </button>
-            <img
-                src="/assets/home/cta-2-illustration.webp"
-                alt=""
-                className="cta2-card__illustration"
-            />
-        </div>
-    );
+  return (
+    <div className="cta2-card ">
+      <div className="cta2-card__text">
+        <h2 className="heading-1">Diversify your Portfolio</h2>
+        <h3 className="heading-5">Collect Real World Assets</h3>
+        <p className="caption-2">
+          Welcome in our high-end ownership and trade platform.
+        </p>
+      </div>
+      <a href="#" className="btn btn-gold">
+        JOIN THE WAITLIST
+      </a>
+    </div>
+  );
 };
 
 export default CTA2Card;
