817cd217d2da00a7209cd14a20421883d093dad4
matt <maweiche@gmail.com>
2024-11-12 12:45:33 +0100

    fixed mobile nav

--- src/components/login/LoginDialog.tsx
+++ src/components/login/LoginDialog.tsx
@@ -111,7 +111,7 @@ export function LoginDialog() {
         </Button>
 
         {isOpen && (
-          <div className="fixed h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-10">
+          <div className="fixed h-screen inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[201]">
             <div 
               className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-10" 
               onClick={handleClose} 
